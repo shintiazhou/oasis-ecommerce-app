8266491f36b985b58be120fa656549a15171c497
shintia zhou <shintiazhouu28@gmail.com>
2020-12-28 16:27:21 +0700

    add withSpinner component

--- src/pages/shop-page/ShopPage.jsx
+++ src/pages/shop-page/ShopPage.jsx
@@ -2,16 +2,25 @@ import React, { useEffect } from "react"
 import CollectionOverview from "../../component/collections-overview/CollectionOverview"
 import { useDispatch } from "react-redux"
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions"
+import { useSelector } from "react-redux"
+import { selectShopisLoading } from "../../redux/shop/shop.selectors"
+import WithSpinner from "../../component/withSpinner/WithSpinner"
 
 const ShopPageComponent = () => {
+    const isLoading = useSelector(state => selectShopisLoading(state))
+
     const dispatch = useDispatch()
+    const WithSpinnerCollectionsOverview = WithSpinner(CollectionOverview)
 
     useEffect(() => {
         dispatch(fetchCollectionsStart())
         window.scrollTo(0, 0)
     }, [dispatch])
+
     return <div >
-        <CollectionOverview />
+        <WithSpinnerCollectionsOverview
+            isLoading={isLoading}
+        />
     </div>
 }
 export default ShopPageComponent
